ed588e62c2e516b528c4e65d2979d69c265a63f7
PodushkaPIR <seterovas@yandex.ru>
2023-12-29 23:25:28 +0000

    strange bufferline and add toggleterm

--- lua/shesh/plugins/bufferline.lua
+++ lua/shesh/plugins/bufferline.lua
@@ -1,11 +1,20 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    version = "*",
+    opts = {
+        options = {
+            mode = "tabs",
+            separator_style = "slant",
+        },
     },
-  },
+    config = function()
+        local keymap = vim.keymap -- for conciseness
+        keymap.set('n', 'gb', '<cmd>BufferLinePick<CR>')
+        keymap.set('n', '<leader>ts', '<cmd>BufferLinePickClose<CR>')
+        keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<CR>')
+        keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<CR>')
+        -- keymap.set('n', ']b', '<CMD>BufferLineMoveNext<CR>')
+        -- keymap.set('n', '[b', '<CMD>BufferLineMovePrev<CR>')
+    end,
 }
